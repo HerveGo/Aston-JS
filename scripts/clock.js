636fe3caab57a91e645a1ad63686a2dd12f956aa
Hervé Gomanne <hgomanne@gmail.com>
2022-01-21 22:11:55 +0100

    Clock working

---
+++ scripts/clock.js
@@ -0,0 +1,40 @@
+/**
+ * Call the main function when the document is fully loaded
+ */
+document.addEventListener("load", main());
+
+/**
+ * Called when the document is loaded
+ */
+function main() {
+    const container = document.getElementById("container");
+    container.appendChild(createClock());
+    const clock = document.getElementById("clock");
+    setInterval(() => updateClock(clock), 1000);
+}
+
+function createClock() {
+    const div = document.createElement("div");
+    div.setAttribute("class", "clock-container");
+        const clock = document.createElement("div");
+        clock.setAttribute("class",  "clock");
+        clock.id = "clock";
+        div.appendChild(clock);
+        updateClock(clock);
+    return div;
+}
+
+function updateClock(clock) {
+    let now = new Date();
+    clock.innerText = `${twoDigits(now.getHours())}:${twoDigits(now.getMinutes())}:${twoDigits(now.getSeconds())}`;
+}
+
+/**
+ * Convert a number in a fixed 2 digits string
+ * @param {number} n an integer between 0 and 99
+ * @returns a string with two digits
+ */
+function twoDigits(n) {
+    return ("0" + n).slice(-2);
+}
+
