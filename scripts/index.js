0c8db0d6ccedb25b150df43ae520478a07a7179d
Hervé Gomanne <hgomanne@gmail.com>
2022-01-22 23:25:10 +0100

    Pure JS Navbar
    Better colors too

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ function main() {
 }
 
 /**
- * Crée et renvoie l'élément <ul> 
+ * Create, fill and return the div containing the cards.
  * @param {Map<string,string>} exo the Map of exercices to do.
  * @returns a HTMLUListElement
  */
@@ -72,33 +72,31 @@ function navigateTo(url) {
  * @param {Map} exercices a Map<String,String>
  */
 function fillExercices(exercices) {
-    exercices.set("Friendship is Magic",`Vous intégrerez une page qui listera les noms des principaux personnages de la série "Friendship is magic", sous forme de liste non ordonnée. 
-    Le texte sera écrit en blanc sous un fond "arc-en-ciel" (gradient) du rose vers le violet.
-    
-    L'affichage sera ENTIEREMENT géré par le script JS de la page : votre page HTML ne doit contenir aucune information si ce n'est un "H1"
-    
-    Vous insérerez un minimum de style afin que ce soit au moins agréable à regarder. 
-    
-    Point de compensation : une image !`);
+    exercices.set("Friendship is Magic",`Vous intégrerez une page qui listera les noms des principaux personnages de la série "Friendship is magic",
+    sous forme de liste non ordonnée. Le texte sera écrit en blanc sous un fond "arc-en-ciel" (gradient) du rose vers le violet.<br>
+    L'affichage sera ENTIEREMENT géré par le script JS de la page : votre page HTML ne doit contenir aucune information si ce n'est un "H1".<br>
+    Vous insérerez un minimum de style afin que ce soit au moins agréable à regarder.<br>
+    Point de compensation : une image !`);
 
-    exercices.set("Horloge",`Vous créerez une page qui affichera l'heure actuelle sous forme : "hh:mm:s" au centre de la page. 
-    Vous insérerez un minimum de style afin que ce soit au moins agréable à regarder. `);
+    exercices.set("Horloge",`Vous créerez une page qui affichera l'heure actuelle sous forme : "hh:mm:ss" au centre de la page. 
+    Vous insérerez un minimum de style afin que ce soit au moins agréable à regarder.`);
 
-    exercices.set("FizzBuzz",`Vous créerez une page qui affichera un champs de texte un compteur allant de 0 à 100. Il affichera : 
-    Le nombre en question, 
-    Fizz si c'est un multiple de 3,
-    Buzz si c'est un multiple de 5
-    FizzBuzz si c'est un multiple de 3 et de 5.
-    
+    exercices.set("FizzBuzz",`Vous créerez une page qui affichera un champs de texte un compteur allant de 0 à 100. Il affichera :
+    <ul>
+        <li>Le nombre en question,</li> 
+        <li>Fizz si c'est un multiple de 3,</li>
+        <li>Buzz si c'est un multiple de 5,</li>
+        <li>FizzBuzz si c'est un multiple de 3 et de 5.</li>
+    </ul>
     Tout votre code sera géré exclusivement par des fonctions.`);
 
-    exercices.set("FizzBuzz++",`Vous créerez un nouveau FizzBuzz, sauf que : 
-
-    Une case affichera le chiffre, 
-    Fizz, Buzz ou FizzBuzz s'afficheront au dessus du chiffre, en grand
-    Les valeurs s'afficheront une à une,
-    Un boutton next permettra de passer à la valeur suivante
-    Un bouton "prev" permettra d'afficher la valeur précédente. 
-    
+    exercices.set("FizzBuzz++",`Vous créerez un nouveau FizzBuzz, sauf que : 
+    <ul>
+        <li>Une case affichera le chiffre,</li> 
+        <li>Fizz, Buzz ou FizzBuzz s'afficheront au dessus du chiffre, en grand,</li>
+        <li>Les valeurs s'afficheront une à une,</li>
+        <li>Un bouton "next" permettra de passer à la valeur suivante,</li>
+        <li>Un bouton "prev" permettra d'afficher la valeur précédente.</li>
+    </ul>
     Recyclage de script : 1 point de récup <3`);
 }
---
+++ scripts/navbar.js
@@ -0,0 +1,64 @@
+class Link {
+    /**
+     * Informations used for a link in the navbar
+     * @param {string} icon the name of the font-awesome to use (fas style)
+     * @param {string} title the title for the link
+     * @param {string} text the text displayed
+     */
+    constructor(icon, title, text) {
+        this.icon = icon;
+        this.title = title;
+        this.text = text;
+    }
+}
+
+/**
+ * Call the main function when the document is fully loaded
+ */
+document.addEventListener("load", createNav());
+
+/**
+ * Starts filling the navbar (when document is ready)
+ */
+function createNav() {
+    const mapNav = new Map();
+    fillMap(mapNav);
+    const nav = document.getElementById("nav");
+    const currentPath = document.location.pathname;
+    mapNav.forEach( (value, key) => {
+        nav.appendChild( navElement(value, key, currentPath) );
+    });
+}
+
+/**
+ * 
+ * @param {Link} value the datas for the link.
+ * @param {string} key the path of the html.
+ * @param {string} currentPath the active path in the navbar.
+ * @returns <p> for the current link, <a> for the others.
+ */
+function navElement(value, key, currentPath) {
+    let navElem;
+    if( "/" + key === currentPath ) {
+        navElem = document.createElement("p");
+        navElem.setAttribute("class", "navactive");
+    } else {
+        navElem = document.createElement("a");
+        navElem.href = key;
+    }
+    navElem.title = value.title;
+    navElem.innerHTML = `<i class="fas ${value.icon}"></i> ${value.text}`;
+    return navElem;
+}
+
+/**
+ * Fill the Map with the informations needed for it to be created.
+ * @param {Map} mapNav an empty map
+ */
+function fillMap(mapNav) {
+    mapNav.set("index.html", new Link(icon="fa-home", title="La page qui vous accueille chaleureusement.", text="Accueil"));
+    mapNav.set("friendship.html", new Link(icon="fa-horse", title="Mon petit poney !", text="Friendship is Magic"));
+    mapNav.set("clock.html", new Link(icon="fa-clock", title="Une fabuleuse Horloge muette.", text="Horloge"));
+    mapNav.set("fizzbuzz.html", new Link(icon="fa-glass-cheers", title="Un Fizz Buzz traditionnel et ennuyeux.", text="Fizz Buzz"));
+    mapNav.set("fizzbuzzplus.html", new Link(icon="fa-plus", title="Un Fizz Buzz interactif extraordinaire !", text="Fizz Buzz +"));
+}
